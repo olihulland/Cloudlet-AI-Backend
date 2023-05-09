3dfb167c324fece7b4facd3eba9503abb9f3d634
Oliver Hulland <olihulland@gmail.com>
2023-05-09 13:11:16 +0100

    basic rest api and serial monitor with handshakes

---
+++ main.py
@@ -0,0 +1,19 @@
+from API import api
+from serial_monitor import SerialMonitor
+import sys
+import threading
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <serial_port>")
+        exit(1)
+
+    debug = True
+
+    # Start the API in a separate thread
+    threading.Thread(target=lambda: api.app.run(debug=debug, use_reloader=False)).start()
+
+    # Start serial monitor in a separate thread
+    serial_port = sys.argv[1]
+    serial_monitor = SerialMonitor(serial_port)
+    serial_monitor.start()
---
+++ serial_monitor.py
@@ -0,0 +1,86 @@
+from serial import Serial
+from threading import Thread
+import string
+import uuid
+
+class SerialMonitor(Thread):
+    def __init__(self, port: str):
+        Thread.__init__(self)
+        self.port = port
+        self.idMap = {}
+        self.deviceMap = {}
+        self.hsMap = {}
+        self.lastID = None
+
+    def generateID(self):
+        printable = string.ascii_letters + string.digits + string.punctuation.replace(",", "").replace(";", "")
+        idOut = ""
+
+        if self.lastID is None:
+            idOut = printable[0] + printable[0]
+        else:
+            lastIndex = printable.index(self.lastID[1])
+            if lastIndex == len(printable) - 1:
+                firstIndex = printable.index(self.lastID[0])
+                idOut = printable[(firstIndex+1) % len(printable)] + printable[0]
+            else:
+                idOut = self.lastID[0] + printable[lastIndex+1]
+
+        self.lastID = idOut
+        return idOut
+
+    def run(self):
+        print("--- RUNNING SERIAL MONITOR ---")
+
+        with Serial(self.port, 115200) as ser:
+            while True:
+                line = ser.readline().decode().strip()
+
+                # handshake to establish an id for transmission
+                if line.startswith("HS"):
+                    commaIndex = line.index(",")
+                    deviceID = line[2:commaIndex]
+                    recordID = line[commaIndex+1:]
+                    
+                    hsID = self.generateID()
+                    uniqueID = str(uuid.uuid4())
+                    
+                    self.idMap[uniqueID] = {"hsID": hsID, "deviceID": deviceID, "recordID": recordID, "message": ""}
+                    self.hsMap[hsID] = uniqueID
+
+                    if (self.deviceMap.get(deviceID) is None):
+                        self.deviceMap[deviceID] = [uniqueID]
+                    else:
+                        self.deviceMap[deviceID].append(uniqueID)
+
+                    message = "HS" + deviceID + "," + hsID + "\n"
+
+                    print(f"HS ID for {deviceID} is {hsID}")
+                    ser.write((message).encode())
+
+                elif len(line) == 0:
+                    continue
+
+                else:
+                    # parse the message
+                    commaIndex = line.index(",")
+                    hsID = line[:2]
+                    seqNum = line[2:commaIndex]
+                    message = line[commaIndex+1:].strip()
+
+                    # check if message include termination character
+                    complete = False
+                    if message.endswith(";"):
+                        complete = True
+                        message = message[:-1]
+                    
+                    # get the unique id
+                    uniqueID = self.hsMap.get(hsID)
+
+                    # append the message
+                    self.idMap[uniqueID]["message"] += message
+
+                    # if the message is complete, send it to the API
+                    if complete:
+                        print("API SEND: " + self.idMap[uniqueID]["message"])
+
